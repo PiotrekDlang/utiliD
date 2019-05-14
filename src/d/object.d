533ed1a247d340299d98eca9fa0ff645d0472205
JinShil <slavo5150@yahoo.com>
2019-05-14 22:14:02 +0900

    Restructure repository and add a first test

---
+++ src/d/object.d
@@ -0,0 +1,6 @@
+// The DMD frontent currently requires a file named object.d to be imported.  So
+// this file exists to fulfill that requirement. It is hoped that DMD can be modified
+// in the fugure to no longer have such an arbitrary requirement.
+module object;
+
+public import types;
---
+++ test/HelloWorld/src/main.d
@@ -0,0 +1,43 @@
+module main;
+
+private extern(C) void __d_sys_exit(long arg1)
+{
+    asm
+    {
+        mov RAX, 60;
+        mov RDI, arg1;
+        syscall;
+    }
+}
+
+private long __d_sys_write(long arg1, in void* arg2, long arg3)
+{
+    long result;
+
+    asm
+    {
+        mov RAX, 1;
+        mov RDI, arg1;
+        mov RSI, arg2;
+        mov RDX, arg3;
+        syscall;
+    }
+
+    return result;
+}
+
+void write(string text)
+{
+    __d_sys_write(2, text.ptr, text.length);
+}
+
+private extern(C) void _start()
+{
+    main();
+    __d_sys_exit(0);
+}
+
+void main()
+{
+    write("Hello, World!");
+}
